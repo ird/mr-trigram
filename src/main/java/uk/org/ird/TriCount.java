4209fdf0916eadb99181efd3e90b12fb02ccfcba
ird <31051194+ird@users.noreply.github.com>
2017-11-21 20:04:39 +0000

    TriCount: completed Map & Reducer classes

--- src/main/java/uk/org/ird/TriCount.java
+++ src/main/java/uk/org/ird/TriCount.java
@@ -5,6 +5,9 @@ import java.util.*;
 
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.conf.*;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapred.*;
 import org.apache.hadoop.util.*;
 
@@ -12,7 +15,8 @@ public class TriCount {
   public static class Map extends MapReduceBase implements Mapper<LongWritable, Text, Text, IntWritable> {
     private final static IntWritable one = new IntWritable(1);
     private Text trigram = new Text();
-    public void map(LongWritable key, Text value, OutputCollector<Text, IntWritable> output, Reporter rep) throws IOException {
+    public void map(LongWritable key, Text value,
+                    OutputCollector<Text, IntWritable> output, Reporter rep) throws IOException {
       String line = value.toString();
       int i = 0;
       while(i < line.length() - 3) {
@@ -22,7 +26,33 @@ public class TriCount {
     }
   }
   public static class Reduce extends MapReduceBase implements Reducer<Text, IntWritable, Text, IntWritable> {
-    public void reduce(Text key, Iterator<IntWritable> 
+    public void reduce(Text key, Iterator<IntWritable> values,
+                       OutputCollector<Text, IntWritable> output, Reporter rep) throws IOException {
+      int sum = 0;
+      while(values.hasNext()){
+        sum += values.next().get();
+      }
+      output.collect(key, new IntWritable(sum));
+    }
+  }
+  public static void main(String[] args) {
+    JobConf job = new JobConf(TriCount.class);
+    job.setJobName("TriCount");
+    job.setOutputKeyClass(Text.class);
+    job.setOutputValueClass(IntWritable.class);
+    job.setMapperClass(Map.class);
+    job.setCombinerClass(Reduce.class);
+    job.setReducerClass(Reduce.class);
+    job.setInputFormat(TextInputFormat.class);
+    job.setOutputFormat(TextOutputFormat.class);
+    FileInputFormat.setInputPaths(job, new Path(args[0]));
+    FileOutputFormat.setOutputPath(job, new Path(args[1]));
+    try {
+      JobClient.runJob(job);
+    } catch (IOException e) {
+      System.out.println(e.getMessage());
+    }
+  }
 }
 
       
